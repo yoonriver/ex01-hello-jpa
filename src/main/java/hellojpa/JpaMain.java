7d14f4bbd901ed90405d75840011beec96645b77
YKS <styleyo12@naver.com>
2021-12-15 18:39:55 +0900

    JPA 기본편 1

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,30 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        
+        // 항상 트랜잭션 안에서 작업을 수행해야 한다.
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            tx.commit();
+        }catch(Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,42 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+public class Member {
+
+    @Id
+    private Long id;
+
+    // 변수명과 컬럼명이 다를 때 컬럼명을 적어주어 매칭해준다.
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    @Transient
+    private int temp;
+
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
+    public Member() {
+
+    }
+
+}
